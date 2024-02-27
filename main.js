2a9f6d6b68164d6a0f733ba00e9dbcbcb7765a23
Claire_GRD <claire-girardin@hotmail.fr>
2024-02-27 18:32:44 +0100

    dernier update

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ import {
   Mesh,
   MeshNormalMaterial,
   AmbientLight,
-  Clock
-} from 'three';
+  Clock,
+} from "three";
 
 // If you prefer to import the whole library, with the THREE prefix, use the following line instead:
 // import * as THREE from 'three'
 
-// NOTE: three/addons alias is supported by Rollup: you can use it interchangeably with three/examples/jsm/  
+// NOTE: three/addons alias is supported by Rollup: you can use it interchangeably with three/examples/jsm/
 
 // Importing Ammo can be tricky.
 // Vite supports webassembly: https://vitejs.dev/guide/features.html#webassembly
@@ -31,20 +31,15 @@ import {
 // But the Ammo lib bundled with the THREE js examples does not seem to export modules properly.
 // A solution is to treat this library as a standalone file and copy it using 'vite-plugin-static-copy'.
 // See vite.config.js
-// 
+//
 // Consider using alternatives like Oimo or cannon-es
-import {
-  OrbitControls
-} from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-import {
-  GLTFLoader
-} from 'three/addons/loaders/GLTFLoader.js';
-import * as THREE from 'three';
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as THREE from "three";
 
 // Example of hard link to official repo for data, if needed
 // const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/js/r148/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
-
 
 // INSERT CODE HERE
 
@@ -52,9 +47,9 @@ import * as THREE from 'three';
 const scene = new Scene();
 const fov = 75; // Camera frustum vertical field of view.
 const aspect = window.innerWidth / window.innerHeight; // Camera frustum aspect ratio.
- const near = 0.01; // Camera frustum near plane.
+const near = 0.01; // Camera frustum near plane.
 const far = 100; // Camera frustum far plane.
-const camera = new PerspectiveCamera(fov, aspect, near, far); 
+const camera = new PerspectiveCamera(fov, aspect, near, far);
 
 const light = new AmbientLight(0xffffff, 1.0); // soft white light
 scene.add(light);
@@ -74,34 +69,32 @@ camera.position.z = 44;
 const loader = new THREE.ObjectLoader();
 
 loader.load(
-	// resource URL
-	"assets/models/scene.json",
+  // resource URL
+  "assets/models/scene.json",
 
-	// onLoad callback
-	// Here the loaded data is assumed to be an object
-	function ( obj ) {
-		// Add the loaded object to the scene
-		scene.add( obj );
-	},
+  // onLoad callback
+  // Here the loaded data is assumed to be an object
+  function (obj) {
+    // Add the loaded object to the scene
+    scene.add(obj);
+  },
 
-	// onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
+  // onProgress callback
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
 
-	// onError callback
-	function ( err ) {
-		console.error( 'An error happened' );
-	}
+  // onError callback
+  function (err) {
+    console.error("An error happened");
+  }
 );
-
 
 // Main loop
 const clock = new Clock();
 
 const animation = () => {
-
-  renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
+  renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR
 
   const delta = clock.getDelta();
   const elapsed = clock.getElapsedTime();
@@ -113,19 +106,16 @@ const animation = () => {
 
 animation();
 
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
 
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-
 }
 
-
-// Object.getObjectByName ( "tresor" ) 
-// Object.getObjectByName ( "bateau" ) 
-// Object.getObjectByName ( "explosif" ) 
-// Object.getObjectByName ( "eau" ) 
+// Object.getObjectByName ( "tresor" )
+// Object.getObjectByName ( "bateau" )
+// Object.getObjectByName ( "explosif" )
+// Object.getObjectByName ( "eau" )
